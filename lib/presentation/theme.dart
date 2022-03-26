16a8c317f6ff6f1e51658b1e1c3cd3ee6bdbe67f
JSRamirez <sebastian@jsramirez.com>
2022-03-25 23:45:05 -0500

    Finish splash Screen

---
+++ lib/presentation/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FoodDeliveryColors {
+  static const orange = Color(0xFFFF4B3A);
+  static const white = Color(0xFFFFFFFF);
+  static const red = Color(0xFFFF460A);
+  static const orangeGradient = Color(0xFFFF470B);
+}
